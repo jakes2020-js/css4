cf95ac1ca2d73c3d7b12707d6b825eaadf789a3e
Jacob Harris <jacobharris@Isaiahs-MacBook-Pro.local>
2020-02-27 18:47:50 -1000

    moved menu a a

--- pet.js
+++ pet.js
@@ -1,14 +1,10 @@
-function toggleSidebar(argument) {
-  document.getElementsById("sidebar").classList.toggle("active");
-}
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("menu").style.marginLeft = "0px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("menu").style.marginLeft = "0";
 }
