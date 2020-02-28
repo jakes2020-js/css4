24c216dbeb382b246a0f01b66eebc2569174c2c6
Jacob Harris <jacobharris@Isaiahs-MacBook-Pro.local>
2020-02-27 16:05:04 -1000

    t

--- pet.js
+++ pet.js
@@ -1,3 +1,14 @@
 function toggleSidebar(argument) {
-	document.getElementsById("sidebar").classList.toggle('active');
+  document.getElementsById("sidebar").classList.toggle("active");
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
